bda2c094b79822c3ea084f34a1d9e081af92a904
Hano <hinsul3113@gmail.com>
2018-11-02 02:46:00 -0700

    created TimersDashboard component

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,36 @@
 
+class TimersDashboard extends React.Component {
+  render(){
+    return(
+      <div className='ui three column centered grid'>
+          <div className='column'>
+              <EditableTimerList />
+              <ToggleableTimerForm
+                isOpen={true}
+              />
+          </div>
+      </div>
+    );
+  }
+}
+
+class EditableTimerList extends React.Component {
+  render(){
+    return(
+      <div>Editable Timer List component</div>
+    );
+  }
+}
+
+class ToggleableTimerForm extends React.Component {
+  render(){
+    return(
+      <div> Toggleable Timer Form component </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <TimersDashboard />,
+  document.getElementById('content')
+);
