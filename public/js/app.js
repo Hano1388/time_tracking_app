cf5221d9543ce532ec57de91d6d0353edd06b275
Hano <hinsul3113@gmail.com>
2018-11-02 18:31:24 -0700

    received props in EditableTimer component and maped through them

--- public/js/app.js
+++ public/js/app.js
@@ -22,10 +22,10 @@ class TimersDashboard extends React.Component {
     return(
       <div className='ui three column centered grid'>
           <div className='column'>
-              <EditableTimerList />
-              <ToggleableTimerForm
-                isOpen={false}
+              <EditableTimerList
+                timers={this.state.timers}
               />
+              <ToggleableTimerForm/>
           </div>
       </div>
     );
@@ -34,22 +34,19 @@ class TimersDashboard extends React.Component {
 
 class EditableTimerList extends React.Component {
   render(){
+    const timers = this.props.timers.map(timer => (
+      <EditableTimer
+        key={timer.id}
+        id={timer.id}
+        title={timer.title}
+        project={timer.project}
+        elapsed={timer.elapsed}
+        runningSince={timer.runningSince}
+      />
+    ));
     return(
       <div id='timers'>
-          <EditableTimer
-            title='Learn React'
-            project='Web Domination'
-            elapsed='8986300'
-            runningSince={null}
-            editFormOpen={false}
-          />
-          <EditableTimer
-            title='Learn extreme ironing'
-            project='World Domination'
-            elapsed='3890985'
-            runningSince={null}
-            editFormOpen={true}
-            />
+        {timers}
       </div>
     );
   }
